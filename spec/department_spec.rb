b684ea917bc7d5068ebcd25f5ebe25f48c6d8df6
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-12 09:21:47 -0700

    test: Department contains an empty array of employees

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -3,12 +3,17 @@ require './lib/employee'
 
  RSpec.describe Department do
  	before(:each) do
- 		@department = Department.new('Customer Service')
+ 		@customer_service = Department.new('Customer Service')
  	end
 
  	it 'is initialized with a name' do
- 		expect(@department).to be_instance_of Department
- 		expect(@department.name).to be_instance_of String
- 		expect(@department.name).to eq 'Customer Service'
+ 		expect(@customer_service).to be_instance_of Department
+ 		expect(@customer_service.name).to be_instance_of String
+ 		expect(@customer_service.name).to eq 'Customer Service'
  	end
+
+  it 'is initialized with an empty array of employees' do
+    expect(@customer_service.employees).to be_instance_of Array
+    expect(@customer_service.employees).to be_empty
+  end
  end
