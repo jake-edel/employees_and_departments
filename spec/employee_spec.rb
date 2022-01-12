0ed53fce94eb3ca51b685b1358b444542b0545ad
Jakob Edelstein <jakobedelstein@gmail.com>
2022-01-12 09:13:25 -0700

    test: Employee contains a name, age, salary

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,11 +1,17 @@
 require './lib/employee'
 
- Rspec.describe Employee do
+ RSpec.describe Employee do
  	before(:each) do
- 		@employee = Employee.new(i)
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
  	end
 
- 	it 'is initialized with something or other' do
- 		expect(@employee).to be_instance_of Employee
+ 	it 'is initialized with a hash of attributes, and can return its name, age, and salary' do
+ 		expect(@bobbi).to be_instance_of Employee
+ 		expect(@bobbi.name).to be_instance_of String
+ 		expect(@bobbi.name).to be eq 'Bobbi Jaeger'
+ 		expect(@bobbi.age).to be_instance_of String
+ 		expect(@bobbi.age).to be eq '30'
+ 		expect(@bobbi.salary).to be_instance_of String
+ 		expect(@bobbi.salary).to be eq '100000'
  	end
- end
+end
